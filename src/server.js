a2053a6995bc2e161d7bb8f2e8081a8d1e6fdacb
Gustavo361 <gustavofaustino361@gmail.com>
2025-05-29 01:09:44 -0300

    init PostgreSQL database config

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 import express from 'express'
 
+import database from './db.js'
+
 // all routes
 import routerHome from './routes/home.js'
 import routerLogin from './routes/login.js'
@@ -7,6 +9,15 @@ import routerSignUp from './routes/signup.js'
 
 const app = express()
 
+async function initDB() {
+    try {
+        await database.query('SELECT NOW()')
+        console.log('database connected successfully:')
+    } catch (err) {
+        console.error('failed to connect to the database.', err)
+        process.exit()
+    }
+}
 
 // Routes
 app.get('/', routerHome)
@@ -19,3 +30,5 @@ app.post('sign-up', routerSignUp)
 app.listen(3000, () => {
     console.log(`server running at 3000`)
 })
+
+initDB()
