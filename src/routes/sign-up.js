832096a8214fe3057d5831d5dd13350920c73646
Gustavo361 <gustavofaustino361@gmail.com>
2025-06-01 00:25:46 -0300

    user data sent to postgreeSQL

--- src/routes/sign-up.js
+++ src/routes/sign-up.js
@@ -1,11 +1,20 @@
 import express from 'express'
+import { createUser } from '../model/user-model.js'
+
 const router = express.Router()
 
-router.post('/sign-up', (req, res) => {
-    const {nickname, email, password} = req.body
-
-    console.log(nickname, email, password)
+router.post('/sign-up', async (req, res) => {
     console.log(req.body)
+    try {
+        const { nickname, email, password } = req.body
+        let user = await createUser(nickname, email, password)
+        console.log('user created successfully!')
+        res.status(200).json(user)
+    } catch (err) {
+        console.error(err)
+        console.log('user creating failed')
+        res.status(500).json({message: 'error signing up user'})
+    }
 })
 
 export default router
